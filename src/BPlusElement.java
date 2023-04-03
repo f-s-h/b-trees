5a45287d9c37ab8783ab9017731b363092840ece
f-s-h <fabian.heinrich@tum.de>
2023-04-03 17:08:55 +0200

    Implemented Constructors, Attributes, Getters and Setters

--- src/BPlusElement.java
+++ src/BPlusElement.java
@@ -1,2 +1,33 @@
-public class BPlusElement {
+public class BPlusElement<T extends Comparable> {
+    private T value;
+    private BPlusElement previous;
+    private BPlusElement next;
+
+    public BPlusElement(T value) {
+        this.value = value;
+    }
+
+    public BPlusElement(T value, BPlusElement previous, BPlusElement next) {
+        this.value = value;
+    }
+
+    public T getValue() {
+        return value;
+    }
+
+    public BPlusElement getPrevious() {
+        return previous;
+    }
+
+    public void setPrevious(BPlusElement previous) {
+        this.previous = previous;
+    }
+
+    public BPlusElement getNext() {
+        return next;
+    }
+
+    public void setNext(BPlusElement next) {
+        this.next = next;
+    }
 }
