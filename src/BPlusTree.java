5d27cd64154e3939e12fedb7899945ea7e355b89
f-s-h <fabian.heinrich@tum.de>
2023-04-03 17:05:48 +0200

    Constructor and Attributes

--- src/BPlusTree.java
+++ src/BPlusTree.java
@@ -1,2 +1,17 @@
 public class BPlusTree {
+    private final int k;
+    private BPlusNode root;
+
+    public BPlusTree(int k) {
+        this.k = k;
+        this.root = null;
+    }
+
+    public void insert() {
+        // TODO
+    }
+
+    public void delete() {
+
+    }
 }
